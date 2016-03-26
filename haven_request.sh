b4e8ee5355acf8e8182696af34199545122800cd
Rodrigo <rgomes@codeblurb.com>
2016-03-26 15:39:36 +0000

    Adding haven API helper scrpit

---
+++ haven_request.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+command="curl -X POST"
+for ((i=3; i <= $#; i++))
+do
+    command="$command --data-urlencode \"${!i}\""
+done
+command="$command \"https://api.havenondemand.com/1/api/$1/$2/v1?apikey=85295d8c-c280-4a6b-9cb3-f62402af1ebc\""
+eval $command 2> /dev/null
