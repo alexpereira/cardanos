54f06b0b3507588917553ab12137fa0e9e5527ff
Alex Pereira <alex.pereira@outlook.com>
2016-03-26 06:06:08 -0400

    latest c

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var googleTranslate = require('google-translate')("AIzaSyCnqDQPbZOSNLPZ8QhyJcdp6LMBSkvO8Zk");
 
+app.use('/assets', express.static('assets') );
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
+//app.use(express.static(__dirname + '/assets'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -22,7 +26,6 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
 
 googleTranslate.translate('Hey man, my name is Alex and I am amazing!', 'es', function(err, translation) {
   console.log(translation.translatedText);
