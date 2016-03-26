8acdda4f1992d1b7eaaf74ad9b0da86d0b7da4d8
Rodrigo <rgomes@codeblurb.com>
2016-03-26 15:08:26 +0000

    User colors, user login/logout notification

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var RColor = require('./rcolor.js')();
 var express = require('express');
 var path = require('path');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var rcolor = new RColor;
 
 var googleTranslate = require('google-translate')("AIzaSyCnqDQPbZOSNLPZ8QhyJcdp6LMBSkvO8Zk");
 
@@ -14,10 +17,24 @@ app.get('/', function(req, res) {
 
 clientList = {}
 io.on('connection', function(socket){
-  clientList[socket.id] = {conn: socket, language: '', nickname: ''};
+  clientList[socket.id] = {conn: socket, language: '', nickname: '', color: rcolor.get(true, 0.3, 0.99)};
   console.log('a user connected');
 
   socket.on('nickname', function(nick){
+      msg = "User " + nick + " has joined.";
+      for(id in clientList) {
+	  var info2 = clientList[id];
+	  if (info2.language == '' || id == socket.id) {
+	      continue
+	  }
+	  a = function(conn) {
+	      googleTranslate.translate(msg, info2.language, function(err, translation) {
+		  conn.emit('chat message', {text: translation.translatedText, bgcolor: "black", txtcolor: "white"});
+	      });
+	  }
+	  a(info2.conn);
+      }
+
     clientList[socket.id].nickname = nick;
   });
 
@@ -40,7 +57,7 @@ io.on('connection', function(socket){
       }
       a = function(conn) {
 	  googleTranslate.translate(msg, info2.language, function(err, translation) {
-              conn.emit('chat message', info.nickname + ": " + translation.translatedText);
+              conn.emit('chat message', {text: info.nickname + ": " + translation.translatedText, bgcolor: info.color, txtcolor: "black"});
 	  });
       }
       a(info2.conn);
@@ -48,6 +65,20 @@ io.on('connection', function(socket){
   });
 
   socket.on('disconnect', function(){
+      nick = clientList[socket.id].nickname;
+      msg = "User " + nick + " has left.";
+      for(id in clientList) {
+	  var info2 = clientList[id];
+	  if (info2.language == '' || id == socket.id) {
+	      continue
+	  }
+	  a = function(conn) {
+	      googleTranslate.translate(msg, info2.language, function(err, translation) {
+		  conn.emit('chat message', {text: translation.translatedText, bgcolor: "black", txtcolor: "white"});
+	      });
+	  }
+	  a(info2.conn);
+      }
     delete clientList[socket.id];
     console.log('user disconnected');
   });
