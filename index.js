687138611cd2dcb66a0e88102ab1dcb44433d639
Alex Pereira <alex.pereira@outlook.com>
2016-03-26 11:07:50 -0400

    latest

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var nickname, language;
-
 var googleTranslate = require('google-translate')("AIzaSyCnqDQPbZOSNLPZ8QhyJcdp6LMBSkvO8Zk");
 
 app.use('/assets', express.static('assets') );
@@ -14,29 +12,47 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+clientList = {}
 io.on('connection', function(socket){
+  clientList[socket.id] = {conn: socket, language: '', nickname: ''};
   console.log('a user connected');
 
   socket.on('nickname', function(nick){
-    nickname = nick;
+    clientList[socket.id].nickname = nick;
   });
 
   socket.on('language', function(lang){
-    language = lang;
+    clientList[socket.id].language = lang;
   });
 
   socket.on('chat message', function(msg){
-    googleTranslate.translate(msg, language, function(err, translation) {
-      io.emit('chat message', nickname + ": " + translation.translatedText);
+/*
+    googleTranslate.detectLanguage(msg, function(err, detection) {
+      clientList[socket.id].language = detection.language;
+      console.log(detection.language);
     });
+*/
+    info = clientList[socket.id];
+    for(id in clientList) {
+      var info2 = clientList[id];
+      if (info2.language == '') {
+    continue
+      }
+      a = function(conn) {
+    googleTranslate.translate(msg, info2.language, function(err, translation) {
+              conn.emit('chat message', info.nickname + ": " + translation.translatedText);
+    });
+      }
+      a(info2.conn);
+    }
   });
 
   socket.on('disconnect', function(){
+    delete clientList[socket.id];
     console.log('user disconnected');
   });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:80');
 });
-
