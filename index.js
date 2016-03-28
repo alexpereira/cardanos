7255cb9818808251a69b7393cc08f66c923b98f4
Rodrigo <rgomes@codeblurb.com>
2016-03-28 02:47:30 +0000

    Fixing bugs, etc.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var io = require('socket.io')(http);
 var execSync = require('exec-sync');
 
 function language(text) {
-    if (Math.random() > 0.2) {
-	return;
-    }
+    // if (Math.random() > 0.2) {
+    // 	return;
+    // }
     console.log("Fetching language");
     command = __dirname + "/haven_request.sh sync identifylanguage \"text=" + text.replace("\"", "\\\"") + "\"";
     json_response = execSync(command);
@@ -17,9 +17,6 @@ function language(text) {
 }
 
 function sentiment(text, lang) {
-    if (Math.random() > 0.2) {
-	return;
-    }
     safe_text = text.replace("\"", "\\\"");
     safe_lang = lang.replace("\"", "\\\"");
     command = __dirname + "/haven_request.sh sync analyzesentiment \"text=" + safe_text + "\" \"language=" + safe_lang + "\"";
@@ -39,6 +36,19 @@ app.get('/', function(req, res) {
 
 HAVEN_MIN_LENGTH=15 // Haven is weird with small messages when detecting language
 clientList = {}
+
+function sendMessage(fromlang, toid, msg, bg, fg, conn) {
+    info = clientList[toid];
+    googleTranslate.translate(msg, fromlang, info.language, function(err, translation) {
+	if (typeof translation != 'undefined') {
+	    msg_to_send = translation.translatedText;
+	} else {
+	    msg_to_send = msg;
+	}
+	conn.emit('chat message', {text: msg_to_send, bgcolor: bg, txtcolor: fg});
+    });
+}
+
 io.on('connection', function(socket){
   clientList[socket.id] = {conn: socket, language: '', nickname: '', color: rcolor.get(true, 0.3, 0.99), auto: true};
   console.log('a user connected');
@@ -50,17 +60,7 @@ io.on('connection', function(socket){
 	  if (info2.language == '' || id == socket.id) {
 	      continue
 	  }
-	  a = function(conn) {
-	      googleTranslate.translate(msg, 'eng', info2.language, function(err, translation) {
-		  if (typeof translation != 'undefined') {
-		      msg_to_send = translation.translatedText;
-		  } else {
-		      msg_to_send = msg;
-		  }
-		  conn.emit('chat message', {text: msg_to_send, bgcolor: "black", txtcolor: "white"});
-	      });
-	  }
-	  a(info2.conn);
+	  sendMessage('eng', id, msg, "black", "white", clientList[id].conn);
       }
 
     clientList[socket.id].nickname = nick;
@@ -79,7 +79,9 @@ io.on('connection', function(socket){
     if (clientList[socket.id].auto && msg.length >= HAVEN_MIN_LENGTH) {
 	new_lang = language(msg);
 	if (new_lang != 'UND') {
+	    sendMessage('eng', socket.id, "Your language was detected as " + new_lang, "black", "white", clientList[socket.id].conn);
 	    clientList[socket.id].language = new_lang;
+	    clientList[socket.id].auto = false;
 	}
     }
 
@@ -103,29 +105,34 @@ io.on('connection', function(socket){
 	  continue
       }
 
-      a = function(conn) {
+      a = function(conn, nickname, msg) {
 	  googleTranslate.translate(msg, info.language, info2.language, function(err, translation) {
 	      if (typeof translation != 'undefined') {
 		  msg_to_send = translation.translatedText;
 	      } else {
 		  msg_to_send = msg;
 	      }
-              conn.emit('chat message', {otext: info.nickname + emot + ": " + msg, text: info.nickname + emot + ": " + msg_to_send, bgcolor: info.color, txtcolor: "black"});
+              conn.emit('chat message', {otext: nickname + emot + ": " + msg, text: nickname + emot + ": " + msg_to_send, bgcolor: info.color, txtcolor: "black"});
 	  });
       }
-      a(info2.conn);
+      a(info2.conn, info.nickname, msg);
     }
   });
 
   socket.on('disconnect', function(){
       nick = clientList[socket.id].nickname;
+      if (nick == '') {
+	  delete clientList[socket.id];
+	  console.log('user disconnected');
+	  return;
+      }
       msg = "User " + nick + " has left.";
       for(id in clientList) {
 	  var info2 = clientList[id];
 	  if (info2.language == '' || id == socket.id) {
 	      continue
 	  }
-	  a = function(conn) {
+	  a = function(conn, msg) {
 	      googleTranslate.translate(msg, 'eng', info2.language, function(err, translation) {
 		  if (typeof translation != 'undefined') {
 		      msg_to_send = translation.translatedText;
@@ -135,7 +142,7 @@ io.on('connection', function(socket){
 		  conn.emit('chat message', {text: msg_to_send, bgcolor: "black", txtcolor: "white"});
 	      });
 	  }
-	  a(info2.conn);
+	  a(info2.conn, msg);
       }
     delete clientList[socket.id];
     console.log('user disconnected');
